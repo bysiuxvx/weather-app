2fa0308b8605b08bb727671687f9d4cd0cbef3c4
Patryk Byszek <byszek.patryk@gmail.com>
2021-03-28 23:25:08 +0200

    the weather app is now giving results

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 class Fetch {
   async getLocation(input) {
     try {
-      const apiKey = 'd48d4a508daae10de3432ba53d9f4175';
+      const myKey = 'd48d4a508daae10de3432ba53d9f4175';
 
       const response = await fetch(
-        `api.openweathermap.org/data/2.5/weather?q={${input}}&appid={${apiKey}}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${myKey}`
       );
 
       const data = await response.json();
-
       console.log(data);
       return data;
     } catch (error) {
@@ -23,7 +22,26 @@ class UI {
     this.city;
   }
 
-  updateUI(data) {
-    this.uiContainer.innerHTML = ``;
+  populateUI(data) {
+    this.uiContainer.innerHTML = `
+    <div class ='cityTemp'>
+    <h5>${data.name}</h5>
+    <h6>${((data.main.temp_max + data.main.temp_min) / 2).toFixed(1)}C.</h6>
+    <p>${data.weather[0].description}</p>
+    </div>`;
   }
 }
+
+const fetchData = new Fetch();
+const updateUI = new UI();
+
+const input = document.querySelector('.searchCity');
+const submitBtn = document.querySelector('.submitBtn');
+
+submitBtn.addEventListener('click', () => {
+  const currentValue = input.value;
+
+  fetchData.getLocation(currentValue).then((data) => {
+    updateUI.populateUI(data);
+  });
+});
