87a8de829c9410d89f48cc575ca4999ec131251c
Patryk Byszek <byszek.patryk@gmail.com>
2021-03-29 03:48:31 +0200

    further changes in most areas

--- app.js
+++ app.js
@@ -24,15 +24,16 @@ class UI {
 
   populateUI(data) {
     this.uiContainer.innerHTML = `
-    <div class ='cityTemp'>
-      <div class='bgc'></div>
-      <h5>${data.name}, ${data.sys.country}</h5>
-      <h6>${((data.main.temp_max + data.main.temp_min) / 2).toFixed(1)}C.</h6>
-      <p>${data.weather[0].description}</p>
-      <img class="city-icon" src='https://openweathermap.org/img/wn/${
-        data.weather[0]['icon']
-      }@2x.png'>
-    </div>`;
+        <div class ='cityTemp'>
+          <h5>${data.name}, ${data.sys.country}</h5>
+          <h6>${((data.main.temp_max + data.main.temp_min) / 2).toFixed(
+            1
+          )}C.</h6>
+          <p>${data.weather[0].description}</p>
+          <img class="city-icon" src='https://openweathermap.org/img/wn/${
+            data.weather[0]['icon']
+          }@2x.png'>
+        </div>`;
   }
 }
 
@@ -47,11 +48,30 @@ const executeSearch = () => {
   fetchData.getLocation(currentValue).then((data) => {
     updateUI.populateUI(data);
   });
+  document.querySelector('.modal').style.display = 'block';
 };
 
+submitBtn.addEventListener('click', executeSearch);
+
+// execute with enter
 document.querySelector('.searchCity').addEventListener('keydown', (event) => {
   if (event.keyCode == 13) {
     executeSearch();
   } else return;
 });
-submitBtn.addEventListener('click', executeSearch);
+
+const closeBtn = document.querySelector('.modal-close');
+const modalBody = document.querySelector('.modal');
+
+// modal close by X
+closeBtn.addEventListener('click', () => {
+  modalBody.style.display = 'none';
+  console.log('test');
+});
+
+// modal target control
+modalBody.addEventListener('click', (event) => {
+  if (event.target == modalBody) {
+    modalBody.style.display = 'none';
+  }
+});
